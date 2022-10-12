ea32b14d80de746cb6077c54641bab0ee86ef482
samanteras <samanter@mail.ru>
2022-10-13 02:48:53 +0300

    Add functions, improve item

--- ToDoList/ListTableViewController.swift
+++ ToDoList/ListTableViewController.swift
@@ -12,54 +12,68 @@ class ListTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
+    @IBAction func pushAddItem(_ sender: Any) {
+        addItem(nameItem: "Name")
+        tableView.reloadData()
+    }
+    
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return toDoList.count
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let currentItem = toDoList[indexPath.row]
+        cell.textLabel?.text = currentItem["Name"] as? String
+        
+        if (currentItem["isComplited"] as? Bool) == true {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
         return cell
     }
-    */
 
-    /*
+
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            removeItem(item: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            
+        }
     }
-    */
+     
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if changeState(index: indexPath.row) {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+    }
 
     /*
     // Override to support rearranging the table view.
--- ToDoList/ModelList.swift
+++ ToDoList/ModelList.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+var toDoList: [[String: Any]] = [["Name": "Позавтракать", "isComplited": true], ["Name": "Найти работу", "isComplited": false], ["Name": "Заработать денег", "isComplited": false]]
+
+
+func addItem(nameItem: String, isCompleted: Bool = false) {
+    toDoList.append(["Name":nameItem, "isComplited": isCompleted])
+    saveData()
+}
+
+func removeItem(item: Int) {
+    toDoList.remove(at: item)
+    saveData()
+}
+
+func changeState(index: Int) -> Bool {
+    toDoList[index]["isComplited"] = !(toDoList[index]["isComplited"] as! Bool)
+    saveData()
+    return toDoList[index]["isComplited"] as! Bool
+}
+
+func saveData(){
+    
+}
+
+func loadData() {
+    
+}
